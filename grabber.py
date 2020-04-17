06e69f9eae376fae45c267e720d5f1fb19de483c
Cameron D <42524057+citren0@users.noreply.github.com>
2020-04-17 11:20:17 -0600

    start

---
+++ grabber.py
@@ -0,0 +1,5 @@
+#all zip and coordinate information from OpenDataSoft
+def menu:
+    print("Geolocation weather finder based on zip code or longitude and lattitude.")
+    choice = input("1. Zip Code \n2.Lattitude Longitude \n3. Quit")
+
